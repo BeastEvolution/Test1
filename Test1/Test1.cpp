6e9759df4acd2721e301962d7ec08dd16bb924c4
BeastEvolution <qp123456ex@gmail.com>
2020-10-02 14:43:25 +0300

    Добавьте файлы проекта.

---
+++ Test1/Test1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//   {}
+class Point
+{
+private:
+
+	int x;
+	int y;
+
+public:
+	int  GetX()
+	{
+		return x;
+	}
+
+	void SetX(int valueX)
+	{
+		x = valueX;
+	}
+
+	void Print()
+	{
+		cout << "X = " << x << "\nY = " << y << endl;
+	}
+};
+
+class Human
+{
+
+};
+
+
+
+
+int main()
+{
+
+	Point a;
+	a.Print();
+
+
+
+	return 0;
+}
+
